44738517383bbe5335e321e59c4a550d54059374
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-04 19:19:01 -0400

    More on Components
    - Added a Product interface
    - Add a custom style  using styleUrls property in Component decorator
    - Try the OnInit LifeCyclehook
    - Built Custom Pipe using Pipe and Pipe Transform
    - Added filter functionality using get & setter methods

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin = 2; //need not mention data type as we have typscript inference
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
+    // cannot filter our products array instead as it requires us to retreieve the data again
+    // from the source. 
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -33,8 +46,23 @@ export class ProductListComponent {
         }
     ];
 
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = '';
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLowerCase();
+        return this.products.filter((product: IProduct) => 
+                product.productName.toLowerCase().indexOf(filterBy) !== -1);
+    }
+
     toggleImage(): void {
           // ! -> Not operator
         this.showImage = !this.showImage;
-    };
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
 }
