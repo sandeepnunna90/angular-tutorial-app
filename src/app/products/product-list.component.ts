e17afcc96b6957782226ad2787c82d38c1238077
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-04 02:56:03 -0400

    Add Bindings to Product List Component
    added:
    - Property Binding to Image element to control show/hide functionality
    - Event Binding to button to toggle between show / Hide button name
    - Two-Way Binding for the input text filter
    
    modified:
    - Update app module to add FormModules to use ngModel for 2-way binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin = 2; //need not mention data type as we have typscript inference
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
         {
             "productId": 2,
@@ -28,4 +32,9 @@ export class ProductListComponent {
             "imageUrl": "assets/images/hammer.png"
         }
     ];
+
+    toggleImage(): void {
+          // ! -> Not operator
+        this.showImage = !this.showImage;
+    };
 }
