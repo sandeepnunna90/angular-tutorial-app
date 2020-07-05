2261953f5f0e78a0435e8a4f1ec9486998dbbb1d
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-05 00:32:35 -0400

    Fetch Product Data from Http
    - Create a method for http get request and include a observable
    -  Subscribe the product-list component to the observable and get the data

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,50 +3,61 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-    pageTitle: string = 'Product List';
-    imageWidth: number = 50;
-    imageMargin = 2; //need not mention data type as we have typscript inference
-    showImage: boolean = false;
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin = 2; //need not mention data type as we have typscript inference
+  showImage: boolean = false;
+  errorMessage: string;
 
-    _listFilter: string;
-    get listFilter(): string {
-        return this._listFilter;
-    }
-    set listFilter(value: string) {
-        this._listFilter = value;
-        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-    }
-    filteredProducts: IProduct[];
-    // cannot filter our products array instead as it requires us to retreieve the data again
-    // from the source. 
-    products: IProduct[] = [];
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
+  }
+  filteredProducts: IProduct[];
+  // cannot filter our products array instead as it requires us to retreieve the data again
+  // from the source.
+  products: IProduct[] = [];
 
-    constructor(private productService: ProductService) {
-        // this.listFilter = '';
-    }
+  constructor(private productService: ProductService) {
+    // this.listFilter = '';
+  }
 
-    onRatingClicked(message: string): void {
-        this.pageTitle = 'Product List: ' + message;
-    }
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
 
-    performFilter(filterBy: string): IProduct[] {
-        filterBy = filterBy.toLowerCase();
-        return this.products.filter((product: IProduct) => 
-                product.productName.toLowerCase().indexOf(filterBy) !== -1);
-    }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
-    toggleImage(): void {
-          // ! -> Not operator
-        this.showImage = !this.showImage;
-    }
+  toggleImage(): void {
+    // ! -> Not operator
+    this.showImage = !this.showImage;
+  }
 
-    ngOnInit(): void {
-        this.products = this.productService.getProducts();
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      // this.products is an instance of class (Product List Component instance)
+      next: (products) => {
+        this.products = products;
         this.filteredProducts = this.products;
-    }
+      },
+      error: (err) => (this.errorMessage = err),
+    });
+  }
 }
